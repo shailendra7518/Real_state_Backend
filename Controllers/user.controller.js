8d6ee699d75a406806e88c6d62885766015310cd
shailendra kumar <shailendraftp000@gmail.com>
2023-10-22 15:57:59 +0530

    delete user features completed

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -3,6 +3,7 @@ const UserModel = require("../Models/user.model");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+const { errorHandler } = require("../utils/errorHandler");
 const userController = {
   getUserById: async (req, res) => {
     try {
@@ -13,10 +14,8 @@ const userController = {
     }
   },
   updateUser: async (req, res, next) => {
- 
-  console.log(req.body,req.params)
+    console.log(req.body, req.params);
     if (req.user.id !== req.params.id) {
-    
       return next("you can update only your account", 401);
     }
     try {
@@ -25,10 +24,9 @@ const userController = {
       // Assuming you have a validation mechanism here to validate the request body
 
       if (req.body.password) {
-      
         req.body.password = await bcrypt.hash(req.body.password, 10);
       }
-      
+
       const updatedUser = await UserModel.findByIdAndUpdate(
         _id,
         {
@@ -41,19 +39,37 @@ const userController = {
         }, // Assuming req.body contains the updated user data
         { new: true } // Return the updated user
       );
-  
 
       if (!updatedUser) {
         return res.status(404).json({ message: "User not found" });
       }
-       const token = jwt.sign({ id: updatedUser._id }, process.env.JWT_SECRET_KEY, {
-         expiresIn: "1h",
-       }); 
+      const token = jwt.sign(
+        { id: updatedUser._id },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
 
-      res
-        .json({status:200, message: "User updated successfully", user: updatedUser,token:token });
+      res.json({
+        status: 200,
+        message: "User updated successfully",
+        user: updatedUser,
+        token: token,
+      });
     } catch (error) {
       next(error);
+    }
+  },
+  deleteUser: async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+      next(errorHandler("you can delte only your own accound",400))
+    }
+    try {
+      const user = await UserModel.findByIdAndDelete(req.params.id);
+      res.json({status:201,message:"user delete successfully",user:user});
+    } catch (error) {
+      res.json({ status:500,message: error.message });
     }
   },
 
